17e60c08bb435141af162ffc192f0c86ffb47e10
Ibrahima DIOP <ibrahima.diop@univ-zig.sn>
2025-01-02 20:32:03 +0000

    correction du nom du dossier controller

---
+++ src/main/java/sn/boom/boutique/controller/VersementController.java
@@ -0,0 +1,43 @@
+package sn.boom.boutique.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sn.boom.boutique.entities.Compte;
+import sn.boom.boutique.entities.Versement;
+import sn.boom.boutique.services.VersementService;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/versements")
+@AllArgsConstructor
+@CrossOrigin("*")
+public class VersementController {
+    private final VersementService versementService;
+
+    @PostMapping
+    public ResponseEntity<Versement> saveVersement(@RequestBody Versement versement) {
+        return ResponseEntity.ok(versementService.saveVersement(versement));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteVersement(@PathVariable Long id) {
+        versementService.deleteVersement(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Versement> getVersement(@PathVariable Long id) {
+        return ResponseEntity.ok(versementService.getVersement(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Versement>> getAllVersements() {
+        return ResponseEntity.ok(versementService.getAllVersements());
+    }
+
+    @GetMapping("/compte")
+    public ResponseEntity<List<Versement>> getVersementsByCompte(@RequestBody Compte compte) {
+        return ResponseEntity.ok(versementService.getVersementsByCompte(compte));
+    }
+}
